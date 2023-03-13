b26bd66a0c44b2f655adb0ff6dc14c53b796f22d
채용운 <>
2023-03-13 10:59:42 +0900

    3/13(월) 오전 업로드 - 부서지는 벽 게이트에 UI 카운트 추가, 머티리얼 설정 - 안튕기고 흘러내리는 벽

--- Assets/01.Scripts/Managers/Game/BreakWall.cs
+++ Assets/01.Scripts/Managers/Game/BreakWall.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class BreakWall : MonoBehaviour
 {
@@ -18,27 +19,43 @@ public class BreakWall : MonoBehaviour
     public float _time = 1f;
     public Vector3 _offset = Vector3.up * 2;
 
+    public Material[] Mat;
+
+    public Text Count_Text;
+
+
     private void Start()
     {
-        _Pices = new Transform[transform.childCount];
+        _Pices = new Transform[transform.childCount - 1];
 
-        for (int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < transform.childCount - 1; i++)
         {
             _Pices[i] = transform.GetChild(i);
+            _Pices[i].GetComponent<Renderer>().sharedMaterial = CanBreak ? Mat[0] : Mat[1];
         }
+
+        if (Count_Text == null)
+        {
+            //Count_Text = transform.GetChild(transform.childCount - 1).GetComponent<Text>();
+            Count_Text = transform.GetComponentInChildren<Text>();
+        }
+
+        Count_Text.gameObject.SetActive(CanBreak);
+
 
     }
 
     [Button]
     public void Explosion()
     {
+        Count_Text.gameObject.SetActive(false);
         foreach (Transform _obj in _Pices)
         {
             _obj.GetComponent<Rigidbody>().isKinematic = false;
             //_obj.GetComponent<Rigidbody>().AddForce(Vector3.one * Random.Range(-1, 1f) * Power);
             _obj.GetComponent<Rigidbody>().AddExplosionForce(Power, transform.position, _Radius);
-
             _obj.transform.DOScale(Vector3.zero, _time);
+
         }
         //Collider[] _cols = Physics.OverlapSphere(transform.position, _Radius);
 
@@ -70,10 +87,10 @@ public class BreakWall : MonoBehaviour
         //              .Append(transform.DOScale(Vector3.one * 1f, 0.1f).SetEase(Ease.Linear));
 
         Current_Count++;
+        Count_Text.text = $"{Break_Count - Current_Count}";
         foreach (Transform _obj in _Pices)
         {
             _obj.transform.localScale = Vector3.one * (1f - 0.1f * (Current_Count / Break_Count));
-
         }
         if (Current_Count > 100)
         {
